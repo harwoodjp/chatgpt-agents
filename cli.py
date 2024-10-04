1ed0138caffef6695349f1187f0a71356755a182
Justin Harwood <justin@harwoodjp.com>
2024-10-04 16:00:26 -0500

    rich output

--- cli.py
+++ cli.py
@@ -1,6 +1,12 @@
 from agents.helpful_assistant import helpful_assistant
+from rich.console import Console
+from rich.markdown import Markdown
 
+console = Console(width=150)
 
 while True:
-  user_input = input('> ')
-  print(f'< {helpful_assistant.message(user_input)}')
+    user_input = input("> ")
+    if len(user_input) == 0:
+        continue
+    response = helpful_assistant.message(user_input)
+    console.print(Markdown(f"< {response}"))
